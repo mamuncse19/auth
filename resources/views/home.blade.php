f2b59a4323dfa8ab26f1f5154e516641f11c9bf7
mamun <mamuncse19@gmail.com>
2020-02-18 21:24:53 +0600

    completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,11 +15,7 @@
                     @endif
 
                     You are logged in!
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                    
                 </div>
                
             </div>
